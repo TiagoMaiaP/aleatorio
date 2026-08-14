60b48d3281f694365406466871212113f0b412ee
TiagoMaiaP <tiagomaiap0@gmail.com>
2026-08-14 10:25:09 -0300

    Criaçao de um novo arquivo para o usuario digitar a idade e nome do mesmo

---
+++ newfile.py
@@ -0,0 +1,5 @@
+nome = str(input("Digite seu nome: "))
+idade = int(input("Digite sua idade: "))
+
+print("Seu nome é: ", nome);
+print("Sua idade é: ", idade);
--- readme.md
+++ readme.md
@@ -1,2 +1,2 @@
-'hello world'
-qualquer coisa .
+
+README
